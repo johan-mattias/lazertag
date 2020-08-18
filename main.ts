0aba269ee2f81b2c638d9c676ab9d720d97ed22b
Johan <jsnider@users.noreply.github.com>
2020-08-18 16:44:48 +0200

    reloading, magazine

--- main.ts
+++ main.ts
@@ -1,51 +1,134 @@
-radio.onReceivedNumber(function on_received_number(receivedNumber: number) {
-    
+radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1 && block == 0) {
         game.removeLife(1)
         life += -1
         basic.showNumber(life)
     } else if (receivedNumber == 1 && block == 1) {
         game.addScore(1)
-    } else {
-        
+        block = 0
     }
-    
-    basic.showNumber(life)
-})
-//  shoot
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    radio.sendNumber(1)
-    basic.showLeds(`
-        . # . . .
-        . # # # #
-        . # . . .
-        . # . . .
-        . . . . .
-        `)
     basic.pause(500)
-    basic.showNumber(life)
+    view()
 })
-//  block
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
-    
-    block = 1
+// block
+input.onButtonPressed(Button.A, function () {
+    if (block_max > 0) {
+        block = 1
+        block_max += -1
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+        basic.pause(5000)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        block = 0
+    } else {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    view()
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showLeds(`
+        . # . . .
+        . # . . .
+        . # . . .
+        . # . . .
+        . # # # .
+        `)
+    basic.pause(3000)
     basic.showLeds(`
         . . . . .
-        . # # # .
-        . # # # .
-        . # # # .
+        . . . . .
+        . . . . .
+        . . . . .
         . . . . .
         `)
-    basic.pause(5000)
-    block = 0
-    basic.showNumber(life)
+    mag = 5
+    view()
 })
+// shoot
+input.onButtonPressed(Button.B, function () {
+    if (mag > 0) {
+        radio.sendNumber(1)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . # . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        mag += -1
+    } else {
+        basic.showLeds(`
+            . # # # .
+            . # . # .
+            . # # # .
+            . # # . .
+            . # . # .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    basic.pause(500)
+    view()
+})
+function view () {
+    for (let plats = 0; plats <= life; plats++) {
+        led.plot(0, plats)
+    }
+    for (let plats = 0; plats <= mag; plats++) {
+        led.plot(4, plats)
+    }
+}
+let block_max = 0
 let block = 0
+let mag = 0
 let life = 0
 radio.setGroup(1)
 game.setLife(3)
-life = 3
-basic.showNumber(life)
-basic.forever(function on_forever() {
-    
-})
+life = 2
+mag = 4
+block = 0
+block_max = 3
+view()
